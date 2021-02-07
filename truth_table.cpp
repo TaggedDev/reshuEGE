03fe93d3cfa092a469fd678e80f9a851d652b51f
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-02-07 17:17:32 +0300

    Edited truth table algorithm
    now works with any amount of variables

--- truth_table.cpp
+++ truth_table.cpp
@@ -3,6 +3,42 @@
 #include <vector>
 using namespace std;
 
+void setV(int i, bool& v) {
+    if (i % 32 <= 16 && i % 32 != 0) {
+        v = true;
+    }
+    else {
+        v = false;
+    }
+}
+
+void setW(int i, bool& w) {
+    if (i % 16 <= 8 && i % 16 != 0) {
+        w = true;
+    }
+    else {
+        w = false;
+    }
+}
+
+void setX(int i, bool& x) {
+    if (i % 8 <= 4 && i % 8 != 0) {
+        x = true;
+    }
+    else {
+        x = false;
+    }
+}
+
+void setY(int i, bool& y) {
+    if (i % 4 == 0 || i % 4 == 1) {
+        y = true;
+    }
+    else {
+        y = false;
+    }
+}
+
 void setZ(int i, bool& z) {
     if (i % 2 == 0) {
         z = true;
@@ -13,40 +49,30 @@ void setZ(int i, bool& z) {
 }
 
 int main() {
-    bool x, y, z;
+    bool w, x, y, z, v;
+    v = false;
+    w = false;
     x = false;
     y = false;
     z = false;
     
-    cout << "x " << "y " << "z" << endl;
+    cout << "     v " << "w " << "x " << "y " << "z" << endl;
 
-    for (int i = 1; i < 5; i++)
+    for (int i = 1; i < 33; i++)
     {
-        x = false;
-        if (i > 2) {
-            y = true;
+        setV(i, v);
+        setW(i, w);
+        setX(i, x);
+        setY(i, y);
+        setZ(i, z);
+        if (i < 10) {
+            cout << i << ".   " << v << " " << w << " " << x << " " << y << " " << z << endl;
         }
         else {
-            y = false;
+            cout << i << ".  " << v << " " << w << " " << x << " " << y << " " << z << endl;
         }
-        setZ(i, z);
-        cout << x << " " << y << " " << z << endl;
+       
     }
-
-    for (int i = 1; i < 5; i++)
-    {
-        x = true;
-        if (i > 2) {
-            y = true;
-        }
-        else {
-            y = false;
-        }
-        setZ(i, z);
-        cout << x << " " << y << " " << z << endl;
-    }
-    
-    
 
     return 0;
 }
